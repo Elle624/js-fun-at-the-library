78853c8b6115f35af610707ac7263bcdd0daf638
Elle Li <yiran.li@hotmail.com>
2020-08-23 19:28:55 -0600

    Refactor checoutBook Function

--- src/Library.js
+++ src/Library.js
@@ -13,31 +13,18 @@ function addBook(libraryName,bookName) {
   libraryName.shelves[bookName.genre].push(bookName)
 }
 
-
 function checkoutBook(libraryName,book) {
-  var fictionShelf = libraryName.shelves.fiction;
-  var fantasyShelf = libraryName.shelves.fantasy;
-  var nonFictionShelf = libraryName.shelves.nonFiction;
+  var totalShelf=libraryName.shelves
 
-  for (var i=0; i<fictionShelf.length; i++){
-    if (book === fictionShelf[i].title) {
-      fictionShelf.splice(i,1);    
-      return `You have now checked out ${book} from the ${libraryName.name}`;
-    }
-  } 
-  for (var i=0; i<fantasyShelf.length; i++) { 
-    if (book === fantasyShelf[i].title) {
-      fantasyShelf.splice(i,1);    
-      return `You have now checked out ${book} from the ${libraryName.name}`;
+  for (var eachShelf in totalShelf){
+    for (var i=0; i<totalShelf[eachShelf].length; i++) {    
+      if (book === totalShelf[eachShelf][i].title) {
+        totalShelf[eachShelf].splice(i,1);    
+        return `You have now checked out ${book} from the ${libraryName.name}`;
+      }
     }
   }
-  for (var i=0; i<nonFictionShelf.length; i++) { 
-   if (book === nonFictionShelf[i].title) {
-      nonFictionShelf.splice(i,1);    
-      return `You have now checked out ${book} from the ${libraryName.name}`;
-    }
-  }
-  return `Sorry, there are currently no copies of ${book} available at the ${libraryName.name}`
+  return `Sorry, there are currently no copies of ${book} available at the ${libraryName.name}`;
 }
 
 module.exports = {
