8e9d978501a840ca077c94dcbb44526b1196b04f
Elle Li <yiran.li@hotmail.com>
2020-08-23 20:40:52 -0600

    Clean Comment Out Code

--- src/librarian.js
+++ src/librarian.js
@@ -28,32 +28,6 @@ class Librarian {
       return `Sorry, we do not have ${bookName}`
     }
 
-  // findBook(bookName) {
-  //   var fantasyShelf = this.library.shelves.fantasy
-  //   var fictionShelf = this.library.shelves.fiction
-  //   var nonFictionShelf = this.library.shelves.nonFiction
-
-  //   for (var i=0; i<fantasyShelf.length; i++){
-  //     if (bookName===fantasyShelf[i].title){
-  //       fantasyShelf.shift()
-  //       return `Yes, we have ${bookName}`
-  //     }
-  //   }
-  //   for (var i=0; i<fictionShelf.length; i++){
-  //     if (bookName===fictionShelf[i].title){
-  //       fictionShelf.shift()
-  //       return `Yes, we have ${bookName}`
-  //     }
-  //   }
-  //   for (var i=0; i<nonFictionShelf.length; i++){
-  //     if (bookName===nonFictionShelf[i].title){
-  //       nonFictionShelf.shift()
-  //       return `Yes, we have ${bookName}`
-  //     }
-  //   }
-  //   return `Sorry, we do not have ${bookName}`
-  // }
-
   calculateLateFee(numOfDate){
     return Math.ceil(numOfDate*0.25)
   }
